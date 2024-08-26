2e67d0ea66bff0e0ded4366df9f014ce8e33bc96
jjlee6496 <ljj3499@gmail.com>
2024-08-27 03:18:49 +0900

    ticker목록 작성

---
+++ tickers.py
@@ -0,0 +1,11 @@
+tickers = {
+    'indices': ['^GSPC', '^NDX', '^GDAXI', '^FTSE', '^FCHI', '^HSI'],
+    'tech': ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'NVDA', 'IBM', 'INTC', 'ORCL', 'CRM'],
+    'finance_energy': ['JPM', 'GS', 'XOM', 'CVX', 'MS', 'WFC', 'BP', 'TOT'],
+    'healthcare': ['JNJ', 'PFE', 'MRK', 'ABBV', 'GILD', 'AMGN', 'NVO', 'SNY', 'LLY'],
+    'consumer': ['PG', 'KO', 'PEP', 'CL', 'KR', 'TGT', 'WBA', 'COST', 'WMT'],
+    'etfs': ['SPY', 'QQQ', 'VTI', 'EEM', 'IWM', 'VEA', 'AGG', 'GLD'],
+    'crypto': ['BTC-USD', 'ETH-USD', 'XRP-USD', 'LTC-USD', 'LINK-USD', 'DOT-USD', 'XLM-USD', 'XTZ-USD'],
+    'others': ['UBER', 'LYFT', 'SHOP', 'SQ', '0241.HK', 'NIO'],
+    'commodities': ['GC=F', 'GLD', 'SI=F', 'SLV', 'CL=F', 'USO', 'NG=F', 'UNG', 'HG=F', 'DBC', 'GSG', 'DBA', 'PL=F', 'PA=F', 'KC=F', 'SB=F', 'CT=F', 'BO=F', 'LB=F']
+}
